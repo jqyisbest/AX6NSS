408654dbaa49876bea35fa4095cba914ca575ad7
JQY <16437320+jqyisbest@users.noreply.github.com>
2024-06-06 17:56:05 +0800

    Update 10_system.js
    Signed-off-by: JQY <16437320+jqyisbest@users.noreply.github.com>

--- luci feed mod/10_system.js
+++ luci feed mod/10_system.js
@@ -86,7 +86,7 @@ return baseclass.extend({
 			_('Model'),            boardinfo.model + cpubench.cpubench,
 			_('Architecture'),     cpuinfo.cpuinfo || boardinfo.system,
 			_('Target Platform'),  (L.isObject(boardinfo.release) ? boardinfo.release.target : ''),
-			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || '') + 'CompiledByJ',
+			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion ? luciversion + ' / ' : '') + 'CompiledByJ',
 			_('Kernel Version'),   boardinfo.kernel,
 			_('Local Time'),       datestr,
 			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
